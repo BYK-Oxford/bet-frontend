55ab148a5f668b41586adef6c2d40876247f941d
Gurung-Manish <manishgrg496@gmail.com>
2025-04-03 18:15:20 +0100

    updated head 2 head ui

--- src/app/match_detail/components/HeadToHeadTab.tsx
+++ src/app/match_detail/components/HeadToHeadTab.tsx
@@ -15,7 +15,7 @@ const HeadToHeadTab: React.FC<{ matches: MatchProps[] }> = ({ matches }) => {
       {matches.map((match, index) => (
         <div
           key={index}
-          className="text-white p-2 flex items-center w-full min-w-[400px] max-w-[700px] justify-between gap-4 border-b border-gray-700"
+          className="text-white p-2 flex items-center w-full min-w-[400px] max-w-[700px] justify-between gap-4 border-b border-[rgba(255,255,255,0.1)]"
         >
           {/* Date */}
           <div className="text-xs text-gray-400 w-20">{match.date}</div>
@@ -23,18 +23,18 @@ const HeadToHeadTab: React.FC<{ matches: MatchProps[] }> = ({ matches }) => {
           {/* Teams Row */}
           <div className="flex items-center justify-center flex-1 gap-3">
             {/* Team 1 */}
-            <div className="flex items-center gap-2 w-28 justify-end">
-              <span className="text-xs truncate">{match.team1}</span>
-              <img src={match.logo1} alt={match.team1} className="w-6 h-6 object-contain p-1" />
+            <div className="flex flex items-center justify-center text-right leading-tight">
+              <span className="text-xs">{match.team1}</span>
+              <img src={match.logo1} alt={match.team1} className="w-14 h-14 object-contain p-1 mt-1" />
             </div>
-
+          
             {/* Score */}
-            <span className="text-sm font-semibold w-12 text-center">{match.score}</span>
+            <span className="text-xs font-semibold w-12 text-center">{match.score}</span>
 
             {/* Team 2 */}
-            <div className="flex items-center gap-2 w-28 justify-start">
-              <img src={match.logo2} alt={match.team2} className="w-6 h-6 object-contain p-1" />
-              <span className="text-xs truncate">{match.team2}</span>
+            <div className="flex flex items-center  justify-center text-left leading-tight">
+              <img src={match.logo2} alt={match.team2} className="w-14 h-14 object-contain p-1 mb-1" />
+              <span className="text-xs">{match.team2}</span>
             </div>
           </div>
         </div>
