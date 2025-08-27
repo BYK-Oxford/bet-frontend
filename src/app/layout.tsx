8f0226501a38628d05943d6d8fba3123c01416cb
Gurung-Manish <manishgrg496@gmail.com>
2025-08-27 13:09:26 +0100

    adding meta index verify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
   title: "Bet Genie",
   description: "Find the best chances to win!",
   icons: {
-    icon: '/smallLogo.png', 
+    icon: "/smallLogo.png",
   },
 };
 
@@ -46,13 +46,18 @@ export default function RootLayout({
             gtag('config', 'G-9VK2YN00N6');
           `}
         </Script>
+
+        {/* site verification for bing */}
+        <meta name="msvalidate.01" content="C5476D18650320E358ECB1C4F2CEA6E9" />
       </head>
       <body>
         <MatchProvider>
           {/* Flex container with responsive layout */}
           <div className="flex min-h-screen flex-col lg:flex-row relative">
             {/* Right section: Header + Main stacked vertically */}
-            <div className="flex-1 flex flex-col lg:ml-[60px] z-10"> {/* Push right on large screens */}
+            <div className="flex-1 flex flex-col lg:ml-[60px] z-10">
+              {" "}
+              {/* Push right on large screens */}
               <Header />
               <Sidebar />
               <main className="flex-1 p-6">{children}</main>
